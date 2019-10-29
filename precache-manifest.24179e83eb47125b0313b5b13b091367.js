f4f6c9c6d715210ee216ac2e5e983a4c37d1347c
Sijey <Popov040391@gmail.com>
2019-10-29 17:07:59 +0200

    Updates

---
+++ precache-manifest.24179e83eb47125b0313b5b13b091367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a225f37703ab9c6c1e2ae8b1d5d1dc2",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "bd72d497547f2240614b",
+    "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "bc9f85e3314b716393f3",
+    "url": "/react_add-todo-form/static/js/2.2ab85a03.chunk.js"
+  },
+  {
+    "revision": "bd72d497547f2240614b",
+    "url": "/react_add-todo-form/static/js/main.ec7de9d8.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
